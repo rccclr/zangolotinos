e582634b1fdd7dac8145a5f6bfc8c7c9ccfb0589
rccclr <rccclr@gmail.com>
2019-07-01 15:27:06 +0200

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,7 +18,10 @@ const Contact = () => {
       <Article>
         <PageHeader title="Contacto" />
         <Content>
-          Si quieres saber más de nosotros, puedes escribirnos a : <Obfuscate email={config.contactEmail} />
+          Si quieres saber más de nosotros, puedes escribirnos a : <Obfuscate email={config.contactEmail} /> o usa el siguiente
+          formulario.
+        </Content>
+        <Form />
       </Article>
     </Main>
   );
